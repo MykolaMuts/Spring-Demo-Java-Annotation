e7a198ed451b99a37fc6ccdd21646184a809cbcf
MykolaMuts <mutsmykola@gmail.com>
2023-05-03 19:59:29 +0200

    Singleton Scope

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -13,10 +13,14 @@ public class App {
 
         ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class);
 
-        //SpeakerService service = new SpeakerServiceImpl();
-
         SpeakerService service = appContext.getBean("speakerService", SpeakerService.class);
 
+        System.out.println(service);
+
         System.out.println(service.findAll().get(0).getFirstName());
+
+        SpeakerService service2 = appContext.getBean("speakerService", SpeakerService.class);
+
+        System.out.println(service2); //it will print exact same object address
     }
 }
