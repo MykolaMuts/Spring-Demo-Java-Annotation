6f9e2fca70b2d0baa663e4666fcc7da13fa373c0
MykolaMuts <mutsmykola@gmail.com>
2023-05-06 16:38:57 +0200

    add Calendar (FactoryBean)

--- src/main/java/com/pluralsight/AppConfig.java
+++ src/main/java/com/pluralsight/AppConfig.java
@@ -4,13 +4,27 @@ import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
 import com.pluralsight.repository.SpeakerRepository;
 import com.pluralsight.service.SpeakerService;
 import com.pluralsight.service.SpeakerServiceImpl;
+import com.pluralsight.util.CalenderFactory;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.*;
+
+import java.util.Calendar;
 
 @Configuration
 @ComponentScan({"com.pluralsight"})
 public class AppConfig {
 
+    @Bean(name = "cal")
+    public CalenderFactory calenderFactory(){
+        CalenderFactory factory = new CalenderFactory();
+        factory.addDays(2);
+        return factory;
+    }
+
+    @Bean
+    public Calendar calendar() throws Exception{
+        return calenderFactory().getObject();
+    }
 //    @Bean(name = "speakerService")
 //    @Scope(value = BeanDefinition.SCOPE_SINGLETON)
 //    public SpeakerService getSpeakerService() {
--- src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
@@ -1,13 +1,19 @@
 package com.pluralsight.repository;
 
 import com.pluralsight.model.Speaker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 @Repository("speakerRepository")
 public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
+
+    @Autowired
+    private Calendar cal;
 
     @Override
     public List<Speaker> findAll() {
@@ -17,6 +23,8 @@ public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
 
         speaker.setFirstName("Mykola");
         speaker.setSecondName("Muts");
+
+        System.out.println("calendar: " + cal.getTime());
 
         speakers.add(speaker);
 
